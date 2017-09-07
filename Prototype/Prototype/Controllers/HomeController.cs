9b61cc8955b0407e0ee8e4ad2cadf491a5db8b19
StephenShirley <stephen1341@gmail.com>
2017-09-06 20:03:53 -0500

    First Prototype
    This is a simple web application that currently takes in an airports shortcode and displays relevant information by calling the MyTSA API.

---
+++ Prototype/Prototype/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;
+using System.Web.Mvc;
+
+namespace Prototype.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+
+        public JsonResult getTsaCheckpoint(string shortcode)
+        {
+            WebClient client = new WebClient();
+            string information = client.DownloadString("http://apps.tsa.dhs.gov/mytsawebservice/GetAirportCheckpoints.ashx?ap=" + shortcode);
+
+            return Json(information, JsonRequestBehavior.AllowGet);
+        }
+        
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Prototype/Prototype/Scripts/Home/APIcalls.js
@@ -0,0 +1,35 @@
+﻿
+//Info is a json object containing flight information.
+//This function will dynamically display the information instead of trying to manually change it all.
+var displayInfo = function (info) {
+    var airportObj = JSON.parse(info)[0].airport;
+    $('#displayBox').html("");
+    $('#displayBox').append('<p><ul>')
+    $('#displayBox').append('<li><strong>Name</strong>: ' + airportObj.name + '</li>');
+    $('#displayBox').append('<li><strong>Location</strong>: ' + airportObj.city + ', ' + airportObj.state + '</li>');
+    $('#displayBox').append('<li><strong>UTC</strong>: ' + airportObj.utc + '</li>');
+    $('#displayBox').append('<li><strong>Precheck</strong>: ' + airportObj.precheck + '</li>');
+
+    $('#displayBox').append('</ul></p>')
+
+    $.each(airportObj, function (index, val) {
+    })
+}
+
+var getTsaCheckpoint = function (shortcodeInput) {
+    $.get("/Home/getTsaCheckpoint", {shortcode: shortcodeInput}, function (data, textStatus, XQHR) {
+        console.log(data);
+        console.log(textStatus);
+        displayInfo(data);
+
+    }).error(function(data, text) {
+        console.log(data);
+    });
+}
+
+
+
+$('body').on('click', '#searchBtn', function () {
+    var input = $('#shortcodeInput').val();
+    getTsaCheckpoint(input);
+})
